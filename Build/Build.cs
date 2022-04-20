bff2212349a2e26a180328e051c72e9fab08ecf4
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-04-19 21:49:51 -0300

    Add ICompileExample

--- Build/Build.cs
+++ Build/Build.cs
@@ -4,8 +4,8 @@ using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
 [CheckBuildProjectConfigurations]
-class Build : NukeBuild, IPublishPack //, ICompileExample
+class Build : NukeBuild, IPublishPack, ICompileExample
 {
-    //string IHazExample.Name => "Example";
+    string IHazExample.Name => "Example";
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
 }
