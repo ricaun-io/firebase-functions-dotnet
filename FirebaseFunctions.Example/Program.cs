fc709e91dd76d49acbd31cfac6b8f0a315c6f9ae
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-04-24 14:35:16 -0300

    Update Example

--- FirebaseFunctions.Example/Program.cs
+++ FirebaseFunctions.Example/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Firebase.Auth;
 using System;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Firebase.Functions.Example
 {
@@ -12,33 +15,125 @@ namespace Firebase.Functions.Example
 
         static void Main(string[] args)
         {
-            var task = Task.Run(FirebaseFunctionsTest);
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Firebase.Functions.Example");
+            Console.WriteLine("--------------------------");
+
+            ReadFirebaseConfig();
+
+            Console.WriteLine("--------------------------");
+
+            var task = Task.Run(AsyncFirebaseFunctionsTest);
             task.GetAwaiter().GetResult();
+
             Console.ReadKey();
         }
 
-        private static async void FirebaseFunctionsTest()
+        private static void ReadFirebaseConfig()
         {
-            Console.WriteLine($"FirebaseFunctionsTest");
-            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(FirebaseApiKey));
+            var firebaseJson = new FirebaseConfig();
+            var firebaseConfigData = firebaseJson.GetFirebaseConfig();
 
-            Console.WriteLine($"SignInAnonymously");
-            var auth = await authProvider.SignInAnonymouslyAsync();
+            FirebaseApiKey = firebaseConfigData.FirebaseApiKey;
+            FirebaseFunctions = firebaseConfigData.FirebaseFunctions;
+            FirebaseCallFunction = firebaseConfigData.FirebaseCallFunction;
 
-            var functions = new FirebaseFunctions(FirebaseFunctions,
-                new FirebaseFunctionsOptions()
-                {
-                    AuthTokenAsyncFactory = () => Task.FromResult(auth.FirebaseToken),
-                });
+            Console.Write($"FirebaseApiKey({Hidden(FirebaseApiKey)}) = ");
+            FirebaseApiKey = ReadLine(FirebaseApiKey);
 
-            var function = functions.GetHttpsCallable(FirebaseCallFunction);
-            Console.WriteLine($"CallAsync: {FirebaseCallFunction}");
+            Console.Write($"FirebaseFunctions({Hidden(FirebaseFunctions)}) = ");
+            FirebaseFunctions = ReadLine(FirebaseFunctions);
 
-            var response = await function.CallAsync();
-            Console.WriteLine($"Response: {response}");
+            Console.Write($"FirebaseCallFunction({FirebaseCallFunction}) = ");
+            FirebaseCallFunction = ReadLine(FirebaseCallFunction);
 
-            Console.WriteLine($"DeleteUser");
-            await authProvider.DeleteUserAsync(auth.FirebaseToken);
+            firebaseConfigData.FirebaseApiKey = FirebaseApiKey;
+            firebaseConfigData.FirebaseFunctions = FirebaseFunctions;
+            firebaseConfigData.FirebaseCallFunction = FirebaseCallFunction;
+
+            firebaseJson.SetFirebaseConfig(firebaseConfigData);
+        }
+
+
+        private static async void AsyncFirebaseFunctionsTest()
+        {
+            try
+            {
+                Console.WriteLine($"FirebaseAuthProvider");
+                var authProvider = new FirebaseAuthProvider(new Auth.FirebaseConfig(FirebaseApiKey));
+
+                Console.WriteLine($"SignInAnonymously");
+                var auth = await authProvider.SignInAnonymouslyAsync();
+
+                var functions = new FirebaseFunctions(FirebaseFunctions,
+                    new FirebaseFunctionsOptions()
+                    {
+                        AuthTokenAsyncFactory = () => Task.FromResult(auth.FirebaseToken),
+                    });
+
+                var function = functions.GetHttpsCallable(FirebaseCallFunction);
+                Console.WriteLine($"CallAsync: {FirebaseCallFunction}");
+
+                var response = await function.CallAsync();
+                Console.WriteLine($"CallResponse: {response}");
+
+                Console.WriteLine($"DeleteAnonymously");
+                await authProvider.DeleteUserAsync(auth.FirebaseToken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception {ex}");
+            }
+        }
+
+        private static string ReadLine(string text)
+        {
+            var content = Console.ReadLine();
+            if (content != "") text = content;
+            return text;
+        }
+        private static string Hidden(string content)
+        {
+            var len = 15;
+            var str = "";
+            if (content.Length > len)
+            {
+                str = content.Substring(0, len);
+                for (int i = 0; i < content.Length - len; i++)
+                    str += "#";
+            }
+            return str;
+        }
+    }
+
+    public class FirebaseConfig
+    {
+        public string FileName => "FirebaseConfig.json";
+        public string Location => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public string GetFilePath() => Path.Combine(Location, FileName);
+
+        public FirebaseConfigData GetFirebaseConfig()
+        {
+            var fileName = GetFilePath();
+            if (File.Exists(fileName))
+            {
+                var content = File.ReadAllText(fileName);
+                return JsonConvert.DeserializeObject<FirebaseConfigData>(content);
+            }
+            return new FirebaseConfigData();
+        }
+
+        public void SetFirebaseConfig(FirebaseConfigData firebaseJsonData)
+        {
+            var fileName = GetFilePath();
+            var content = JsonConvert.SerializeObject(firebaseJsonData);
+            File.WriteAllText(fileName, content);
+        }
+        public class FirebaseConfigData
+        {
+            public string FirebaseApiKey { get; set; } = "";
+            public string FirebaseFunctions { get; set; } = "";
+            public string FirebaseCallFunction { get; set; } = "";
         }
     }
 }
